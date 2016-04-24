d5b18ec444b908cedceb322d330df2a270cd7414
Pratomo Ardianto <ardnet@Pratomos-MacBook-Air.local>
2016-04-24 15:31:16 -0700

    Adding mongodb with the connection, and some other pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Hello World. */
+router.get('/helloworld', function(req, res, next) {
+  res.render('helloworld', {title: 'Hello World!'});
+});
+
+/* GET Userlist page */
+router.get('/userlist', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e, docs) {
+    res.render('userlist', {
+      'userlist': docs
+    });
+  });
+});
+
+/* GET new user page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', {title: 'Add new user'});
+});
+
 module.exports = router;
