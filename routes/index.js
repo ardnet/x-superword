c804a549c511c7ba477e956e95215a2102f1b378
Pratomo Ardianto <ardi@x-team.com>
2016-04-24 16:54:30 -0700

    Add page for adding new data

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,33 @@ router.get('/newuser', function(req, res) {
   res.render('newuser', {title: 'Add new user'});
 });
 
+/* POST to Add user service */
+router.post('/adduser', function(req, res) {
+  // Set our internal DB variable.
+  var db = req.db;
+
+  // Get our form values. These rely on the 'name' attributes.
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // Set our collection.
+  var collection = db.get('usercollection');
+
+  // Submit to the DB.
+  collection.insert({
+    'username': userName,
+    'email': userEmail
+  }, function(err, doc) {
+    if (err) {
+      // If it failed, return error.
+      res.send('There was a problem submitting data.');
+    }
+    else {
+      // And forward to success page.
+      res.redirect('userlist');
+    }
+  });
+
+});
+
 module.exports = router;
