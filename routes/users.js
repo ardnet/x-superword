8f31f6924f40de7da73e74f687c0b4dc24acd2a8
Pratomo Ardianto <ardi@x-team.com>
2016-04-24 17:27:26 -0700

    Add JSON GET for showing user list

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/userlist', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e, docs) {
+    res.json(docs);
+  });
+});
+
 module.exports = router;
